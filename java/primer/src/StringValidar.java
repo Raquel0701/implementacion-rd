86bd6ac7636246bcfad9b8ac55a2346182333082
Raquel0701 <diana.diaz7196@gmail.com>
2023-12-23 11:03:58 -0500

    init

---
+++ java/primer/src/StringValidar.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class StringValidar {
+    public static void main(String[] args) {
+        String curso = "soutv";
+        System.out.println("curso = " + curso);
+        System.out.println(curso.toUpperCase());
+        System.out.println("curso.length() = " + curso.length());
+    }
+}
